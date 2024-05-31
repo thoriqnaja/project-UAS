28a7b24476b08b3a58b395c9ebbaf2a2dbce6845
thoriqnaja <muhammad.thoriqun.2103226@students.um.ac.id>
2024-05-31 23:07:19 +0700

    Update hitung.py

--- jantung.py
+++ jantung.py
@@ -1,7 +1,12 @@
 import streamlit as st
-st.title(prediksi sakit jantung)
 
-umur = st.number_input ("masukkan nilai")
-keluhan = st.number_input ("masukkan keluhan")
+st.title(hitung luas persegi panjang)
 
+panjang = st.number_input ("masukkan nilai", 0)
+lebar = st.number_input ("masukkan lebar", 0)
+hitung = st.button ("hitung luas")
+
+if hitung :
+  luas = panjang * lebar
+  st.write ("luas persegi panjang adalah = ", luas)
 
