7e077aa9713c53e065a11b88edf013343e709d24
thoriqnaja <muhammad.thoriqun.2103226@students.um.ac.id>
2024-06-01 00:29:03 +0700

    Update data.py

--- data.py
+++ data.py
@@ -9,3 +9,19 @@ age = st.text_input ('umur')
 sex = st.text_input ('jenis kelamin')
 
 chol = st.text_input ('kolestrol')
+
+#code for prediction
+hearth_diagnosis =''
+
+#membuat tombol prediksi
+if st.button ('prediksi penyakit jantung'):
+  heart_prediction = model.predict([[age,sex,chol]])
+  
+  if (heart_prediction [0]==1)
+    heart_diagnosis = 'pasien terkena penyakit jantung'
+  else :
+    heart_diagnosis = 'pasien tidak terkena penyakit jantung'
+st.success(heart_diagnosis)
+  
+  
+  
