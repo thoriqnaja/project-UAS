63cb8b10f62604fd7c6c0325361bb86f8a470fa9
thoriqnaja <muhammad.thoriqun.2103226@students.um.ac.id>
2024-05-31 23:02:18 +0700

    Update jantung.py

--- jantung.py
+++ jantung.py
@@ -1,34 +1,7 @@
-import warnings
-import os
+import streamlit as st
+st.title(prediksi sakit jantung)
 
-import numpy as np  # NumPy for numerical computing
-import pandas as pd  # Pandas for data manipulation and analysis
-
-# Data visualization libraries
-import seaborn as sns
-import matplotlib.pyplot as plt
-import plotly.express as px
-
-warnings.simplefilter(action='ignore', category=Warning)
-
-# Some Pandas settings
-pd.set_option('display.max_columns', None)
-pd.set_option('display.float_format', lambda x: '%.3f' % x)
-pd.set_option('display.width', 500)
-df = pd.read_csv("/kaggle/input/d/mexwell/heart-disease-dataset/heart_statlog_cleveland_hungary_final.csv")
-def check_df(dataframe):
-    print("##################### Row and Column Count #####################")
-    print(dataframe.shape)
-    print("\n##################### Column Names #####################")
-    print(dataframe.columns)
-    print("\n##################### First Five Rows #####################")
-    print(dataframe.head())
-    print("\n##################### Last Five Rows #####################")
-    print(dataframe.tail())
-    print("\n##################### DataFrame Information #####################")
-    dataframe.info()
-    print("\n##################### Data Types #####################")
-    print(dataframe.dtypes)
+umur = st.number_input ("masukkan nilai")
+keluhan = st.number_input ("masukkan keluhan")
 
 
-check_df(df)
